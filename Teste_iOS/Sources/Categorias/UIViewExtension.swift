8b338cfb7ae6aa6f9fa43be23dec5843c9e05bfb
andrecampopiano <andre.campopiano@icloud.com>
2017-03-29 01:22:31 -0300

    inicio projeto

---
+++ Teste_iOS/Sources/Categorias/UIViewExtension.swift
@@ -0,0 +1,23 @@
+//
+//  UIViewExtension.swift
+//  Teste_iOS
+//
+//  Created by André Campopiano on 29/03/17.
+//  Copyright © 2017 André Campopiano. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func addConstraintsWithFormat(format: String, views: UIView...) {
+        var viewsDictionary = [String: UIView]()
+        for (index, view) in views.enumerated() {
+            let key = "v\(index)"
+            view.translatesAutoresizingMaskIntoConstraints = false
+            viewsDictionary[key] = view
+        }
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
+    }
+}
+
---
+++ Teste_iOS/Sources/Components/HTMSalesCell.swift
@@ -0,0 +1,24 @@
+//
+//  HTMSalesCell.swift
+//  Teste_iOS
+//
+//  Created by André Campopiano on 28/03/17.
+//  Copyright © 2017 André Campopiano. All rights reserved.
+//
+
+import UIKit
+
+class HTMSalesCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
